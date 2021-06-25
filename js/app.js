d2ff79662a6de07827c728f8459c6bcbf4eb1156
kelvinrandu <randukelvin@gmail.com>
2021-06-25 12:40:32 +0300

    logical operators

--- js/app.js
+++ js/app.js
@@ -8,4 +8,127 @@ const greet=()=>{
 }
 console.log(greet())
 console.log(greeting)
-const greetings = document.write('adddd')
+
+
+/*  operators */
+document.write('operators')
+let a= 30;
+let b= 30;
+
+b--;
+a++;
+
+var linebreak = "</br>";
+document.write(linebreak)
+document.write(a)
+document.write(linebreak)
+document.write(b)
+ 
+//addition
+document.write(linebreak)
+let result = a+b;
+document.write(result)
+
+//subtraction
+document.write(linebreak)
+result = a-b;
+document.write(result)
+
+//multiplication
+document.write(linebreak)
+result = a*b;
+document.write(result)
+
+//division
+document.write(linebreak)
+result = a/b;
+document.write(result)
+
+//modulus
+document.write(linebreak)
+result = a%b;
+document.write(result)
+document.write(linebreak);
+
+/*  comparison operators */
+ 
+document.write("comparison operators ");
+      
+document.write("(a == b) => ");
+result = (a == b);
+document.write(result);
+document.write(linebreak);
+         
+document.write("(a < b) => ");
+result = (a < b);
+document.write(result);
+document.write(linebreak);
+         
+document.write("(a > b) => ");
+result = (a > b);
+document.write(result);
+document.write(linebreak);
+         
+document.write("(a != b) => ");
+result = (a != b);
+document.write(result);
+document.write(linebreak);
+         
+document.write("(a >= b) => ");
+result = (a >= b);
+document.write(result);
+document.write(linebreak);
+document.write("(a <= b) => ");
+result = (a <= b);
+document.write(result);
+document.write(linebreak);
+
+/*  logical operators */
+document.write("logical operators");
+document.write(linebreak);
+document.write("(a && b) => ");
+result = (a && b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a || b) => ");
+result = (a || b);
+document.write(result);
+document.write(linebreak);
+
+document.write("!(a && b) => ");
+result = (!(a && b));
+document.write(result);
+document.write(linebreak); 
+
+
+/* bitwise operator */
+document.write("bitwise operator");
+
+document.write("(a & b) => ");            
+result = (a & b);           
+document.write(result);
+document.write(linebreak);
+document.write("(a | b) => ");
+result = (a | b);
+document.write(result);
+document.write(linebreak);
+document.write("(a ^ b) => ");
+result = (a ^ b);
+document.write(result);
+document.write(linebreak);
+         
+document.write("(~b) => ");
+result = (~b);
+document.write(result);
+document.write(linebreak);
+         
+document.write("(a << b) => ");
+result = (a << b);
+document.write(result);
+document.write(linebreak);
+         
+document.write("(a >> b) => ");
+result = (a >> b);
+document.write(result);
+document.write(linebreak);
