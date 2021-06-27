339ccce79468622fbf51b7c4611482c63290fb9a
kelvinrandu <randukelvin@gmail.com>
2021-06-27 15:54:03 +0300

    added javascript clock

--- js/app.js
+++ js/app.js
@@ -1,56 +1,29 @@
-const timer = document.getElementById('stopwatch');
-
-var hr = 0;
-var min = 0;
-var sec = 0;
-var stoptime = true;
-
-function startTimer() {
-  if (stoptime == true) {
-        stoptime = false;
-        timerCycle();
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
     }
-}
-function stopTimer() {
-  if (stoptime == false) {
-    stoptime = true;
-  }
-}
-
-function timerCycle() {
-    if (stoptime == false) {
-    sec = parseInt(sec);
-    min = parseInt(min);
-    hr = parseInt(hr);
-
-    sec = sec + 1;
-
-    if (sec == 60) {
-      min = min + 1;
-      sec = 0;
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
     }
-    if (min == 60) {
-      hr = hr + 1;
-      min = 0;
-      sec = 0;
-    }
-
-    if (sec < 10 || sec == 0) {
-      sec = '0' + sec;
-    }
-    if (min < 10 || min == 0) {
-      min = '0' + min;
-    }
-    if (hr < 10 || hr == 0) {
-      hr = '0' + hr;
-    }
-
-    timer.innerHTML = hr + ':' + min + ':' + sec;
-
-    setTimeout("timerCycle()", 1000);
-  }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("showClock").innerText = time;
+    document.getElementById("showClock").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
 }
 
-function resetTimer() {
-    timer.innerHTML = '00:00:00';
-}
+showTime();
